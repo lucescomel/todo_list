60d5e502c6c93d13cf6e35430f68f923acbcc171
lucescomel <lucescomel@outlook.com>
2026-03-09 11:48:48 +0100

    Add sentry (front)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { initSentry } from './sentry.js'
+
+initSentry()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
